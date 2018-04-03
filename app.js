b8ba69e52b879611ca796165a31764bf94860c8a
epattc <epattc@gmail.com>
2018-04-03 13:10:33 +1000

    Reverting back to IP

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
 
     connection(mysql,{
 
-        host: 'mysql.service.host', // host mysql service
+        host: '172.30.210.54', // host mysql service
         user: 'dbuser',
         password : 'password',
         port : 3306, // port mysql service
